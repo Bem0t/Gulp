aed8e3531ad3676837902bc9e001beacb1df5e5c
Bem0t <71875399+Bem0t@users.noreply.github.com>
2022-03-20 14:54:16 +0300

    2
    2

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,14 +1,16 @@
 /* Проверка поддержки webp, добавление класса webp или no-webp для html */
-export function testWebP(callback) {
+export function isWebp() {
+  function testWebP(callback) {
     /* Проверка поддержки webp */
     var webP = new Image();
     webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
+      callback(webP.height == 2);
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-/* добавление класса _webp или no-webp для html */
-testWebP(function (support) {
+  }
+  /* добавление класса _webp или no-webp для html */
+  testWebP(function (support) {
     let className = support === true ? 'webp' : 'no-webp';
     document.documentElement.classList.add(className);
-});
+  });
+}
